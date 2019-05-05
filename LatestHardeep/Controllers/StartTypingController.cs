7513cca349ddb3099bd6f11d685e0bb9be69bc8a
gaurav5465 <gauravpatiala@outlook.com>
2019-05-05 13:35:28 +0530

    added start typing controller and vides modified start typing controller

---
+++ LatestHardeep/Controllers/StartTypingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LatestHardeep.Controllers
+{
+    public class StartTypingController : Controller
+    {
+        // GET: StartTyping
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+        public ActionResult LearnTyping()
+        {
+            return View("LearnTyping");
+        }
+
+        public ActionResult PracticeTyping()
+        {
+            return View("PracticeTyping");
+        }
+    }
+}
