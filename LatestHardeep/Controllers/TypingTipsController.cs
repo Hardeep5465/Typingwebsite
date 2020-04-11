7d705d1aa6d81781a70380ab068b076cd01f3323
gaurav5465 <gauravpatiala@outlook.com>
2020-04-11 17:46:23 +0530

    Typing Tips page added

---
+++ LatestHardeep/Controllers/TypingTipsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LatestHardeep.Controllers
+{
+    public class TypingTipsController : Controller
+    {
+        // GET: TypingTips
+        public ActionResult Tips()
+        {
+            return View("Tips");
+        }
+    }
+}
