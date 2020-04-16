fd96fe5cbf7d416ee9aa17f7538ca5deb6cf08f1
gaurav5465 <gauravpatiala@outlook.com>
2020-04-16 13:13:05 +0530

    Passage one typing page completed, also graphs UI across pages is modified.

---
+++ LatestHardeep/Controllers/PassageTypingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LatestHardeep.Controllers
+{
+    public class PassageTypingController : Controller
+    {
+        // GET: PassageTyping
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult One()
+        {
+            return View("One");
+        }
+
+    }
+}
